b55a5e2b6dd11a6c2cde5bc28a098c5a0c3977c9
Hoefkens Jeroen <hoefkens.j@gmail.com>
2022-02-07 08:20:05 +0100

    forgot

--- lib/fs.py
+++ lib/fs.py
@@ -112,92 +112,23 @@ def ls_files(path):
 		f=''
 	return f
 	
-def ls_blockdev(**k) -> dict:
+
+def ls_btrfsvol():
+
+def ls_blockdevs(**k) -> dict:
 	"""
 	:return: list of storage devices
 	"""
-	def stat_blkdev_parts(blkdev_part):
-		if 'nvme' in blkdev_part:
-			blockdev=blkdev_part.split('p')[0]
-		elif 'sd' in blkdev_part or 'hd' in blkdev_part:
-			blockdev= blkdev_part[:3]
-		else:
-			return 'ERROR'
-		cmd=f'lsblk --pairs --all --fs --include 259,8 /dev/{blkdev_part}'
-		result= subprocess_tee.run(cmd , tee=False)
-		blkdevs[blockdev]=result.stdout
-		
-	def calc_colwidths(header):
-			colwidths=[]
-			prevchar=' '
-			col_width=0
-			for char in header:
-				if not str(char).isspace():
-					if prevchar.isspace()
-						colwidths+=[col_width]
-						colwidth=0
-					col_width+=1
-					prevchar=char
-				else:
-					col_width+=1
-					prevchar=char
-				
-	
-	def ls_blkdev_disks(**k):
-		blkdev_disks={}
-		filter=k.get('filter') if k.get('filter') else 'TYPE'
-		filterval=k.get('filterval') if k.get('filterval') else 'DISK'
-		lsblk_colums=['TYPE','NAME','PATH']
-		if filterval not in lsblk_colums:
-			lsblk_colums=[filterval,*lsblk_colums]
-			lsblk_sort=f'--sort {filter}'
-		else:
-			lsblk_colums=[filterval, *[col for col in lsblk_colums if col != filterval]]
-			lsblk_sort=f'--sort TYPE'
-		
 
-		cmd = 'lsblk' #259 = maj:NVME , 8 =maj:scsi/sata
-		cmd_args_default='--include 259,8 --all --list'
-		cmd_args_colums='-o '+ str([f'{col},' for col in lsblk_colums][:-1])
-		stdout_lsblk=subprocess.run(shlex.split(f'{cmd} {cmd_args_default} {cmd_args_colums}'), stdout=subprocess.PIPE).stdout.decode('utf-8')
-		calc_colwidhts(stdout_lsblk.split('\u000A')[0])
 	
-		for row in stdout_lsblk.split('\u000A'):
-			
-			if row.casefold().startswith(filterval.casefold())
-				blockdev={'NAME':	row[5:].split('\u0020')[0],
-									'PATH':	row[5:].split('\u0020')[-1],}
-				blockdevs[blockdev['NAME']]= {**blockdev}
-		return blockdevs
-	
-	def ls_blkdev_parts(blockdev, blkdevs):
-		cmd=f'lsblk --pairs --all --fs --include 259,8 /dev/{blockdev}'
-		result=subprocess_tee.run(cmd , tee=False)
-		blkdevs[blockdev]=result.stdout.splitlines()
-		return blkdevs
-	
-	if k.get('DEV') is not None:
-		blkdevs=stat_blkdev_parts(k.get('DEV'))
-	
-	blkdevs = ls_blkdev_disks()
-	for blockdev in blkdevs:
-		blkdevs = ls_blkdev_parts(blockdev, blkdevs)
-		
 
-	named_blockdevs={}
-	for blockdev, value in blkdevs.items():
-		named_blockdevs[blockdev]={}
-		for lst_dev in value:
-			dev=lst_dev.split()[0].split('=')[1].strip('"')
-			named_blockdevs[blockdev][dev]={}
-			for propset in lst_dev.split():
-				key = propset.split('=')[0]
-				val= propset.split('=')[-1]
-				if key != val:
-					val =propset.split('=')[1]
-					named_blockdevs[blockdev][dev][key]=val.strip('"')
-	return named_blockdevs
-	
+
+	cmd = 'lsblk' #259 = maj:NVME , 8 =maj:scsi/sata
+	cmd_args_default='--include 259,8 --all --list -o TYPE,NAME'
+	stdout_lsblk=subprocess.run(shlex.split(f'{cmd} {cmd_args_default}'), stdout=subprocess.PIPE).stdout.decode('utf-8')
+	print(stdout_lsblk)
+
+
 def ls_pyod(path, flags=''):
 	"""
 	similar to the systems 'ls -A' lists directory contents
@@ -213,16 +144,7 @@ def ls_pyod(path, flags=''):
 	other 	=	[o for o in other if not o.startswith('__')]
 	return 		[pyscrs,other,dirs]
 	
-def ls_blkpart(**k)->dict:
-	matches= {}
-	FSTYPE= k.get('FSTYPE') if k else None
-	blkdevs=ls_blockdev()
-	for blkdev in blkdevs.values():
-		for dev in blkdev.values():
-			for val in  dev.values():
-				if FSTYPE is not None and val.casefold() == FSTYPE.casefold():
-					matches[list(blkdev.keys())[0]]=blkdev[dev["NAME"]]
-	return matches
+
 
 
 def renumerate(path):
@@ -241,3 +163,4 @@ def touch(fname, mode=0o666, dir_fd=None, **k):
 		dir_fd=None if os.supports_fd else dir_fd, **k)
 	return os.path.abspath(fname)
 
+ls_blockdevs()
